0879ba541f8c1b47f24bc7e1eebad3a16bf51663
nathanaelcheramlak <nathanaelcheramlak7@gmail.com>
2023-09-07 00:37:04 +0300

    Python

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module for LockedClass"""
+
+
+class LockedClass:
+    """Defines LockedClass"""
+
+    __slots__ = ["first_name"]
+
+    def __init__(self):
+        """Constructor"""
+        pass
