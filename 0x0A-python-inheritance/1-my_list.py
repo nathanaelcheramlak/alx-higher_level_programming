9216881eb665d4d883727cf351de4fc31ae9a810
nathanaelcheramlak <nathanaelcheramlak7@gmail.com>
2023-09-13 20:43:46 +0300

    Python

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module for MyList"""
+
+
+class MyList(list):
+    """ Class that inherits the attributes references of class list
+
+    Args:
+        list: class list
+
+    """
+
+    def print_sorted(self):
+        """ Method that prints the sorted list """
+        l_sorted = self.copy()
+        l_sorted.sort()
+        print(l_sorted)
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module for BaseGeometry"""
+
+
+class BaseGeometry:
+    """ Empty class """
+    def area(self):
+        raise Exception("area() is not implemented")
